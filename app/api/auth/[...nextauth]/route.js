804c2574c858fe73e661a2b99c1f6d5e4939eeaa
saifmohamedsv <xcyberghostx1@gmail.com>
2023-08-04 16:48:48 +0300

    fix session code

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -13,31 +13,31 @@ const handler = NextAuth({
 
   callbacks: {
     async session({ session }) {
+      // store the user id from MongoDB to session
       const sessionUser = await User.findOne({ email: session.user.email });
-      session.user.id = sessionUser?._id.toString();
+      session.user.id = sessionUser._id.toString();
 
       return session;
     },
-
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
 
-        // check if user already exist
-        const userExist = await User.findOne({ email: profile.email });
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-        // if not, create a new one
-        if (!userExist) {
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
           await User.create({
+            email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
             image: profile.picture,
-            email: profile.email,
           });
         }
 
         return true;
       } catch (error) {
-        console.log(error);
+        console.log("Error checking if user exists: ", error.message);
         return false;
       }
     },
